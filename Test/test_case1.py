42c7ba560771f86da6704f84b329d8f4ce0003ff
pramilaravi <pramilaeceian@gmail.com>
2024-06-17 21:04:12 -0500

    commiting the files

---
+++ Test/test_case1.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from Utilities.BaseClass import BaseClass
+
+class Test_Case1(BaseClass):
+
+    def test_openbrowser(self):
+        self.driver.find_element(By.ID,"ra_userName").send_keys("pramila")
